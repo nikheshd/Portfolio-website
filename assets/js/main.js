2f8afade2bbeccf4de767954d083054f7f61dacd
nikheshd <nikheshd@iitk.ac.in>
2023-01-01 18:34:47 +0530

    project details

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,28 +60,50 @@ sr.reveal('.skills__data, .work__img, .contact__input',{interval: 100});
 
 
 //modal
-// Get the modal
+
 var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
 
-// Get the button that opens the modal
 var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var close1 = document.getElementById("close1");
+var close2 = document.getElementById("close2");
+var close3 = document.getElementById("close3");
+var close4 = document.getElementById("close4");
+var close5 = document.getElementById("close5");
+var close6 = document.getElementById("close6");
 
 // When the user clicks on the button, open the modal
-btn1.onclick = function() {
-  modal1.style.display = "block";
-}
+btn1.onclick = function() {modal1.style.display = "block";}
+btn2.onclick = function() {modal2.style.display = "block";}
+btn3.onclick = function() {modal3.style.display = "block";}
+btn4.onclick = function() {modal4.style.display = "block";}
+btn5.onclick = function() {modal5.style.display = "block";}
+btn6.onclick = function() {modal6.style.display = "block";}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal1.style.display = "none";
-}
+close1.onclick = function() {modal1.style.display = "none";}
+close2.onclick = function() {modal2.style.display = "none";}
+close3.onclick = function() {modal3.style.display = "none";}
+close4.onclick = function() {modal4.style.display = "none";}
+close5.onclick = function() {modal5.style.display = "none";}
+close6.onclick = function() {modal6.style.display = "none";}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
+  if (event.target == modal1) modal1.style.display = "none";
+  else if(event.target==modal2) modal2.style.display = "none";
+  else if(event.target==modal3) modal3.style.display = "none";
+  else if(event.target==modal4) modal4.style.display = "none";
+  else if(event.target==modal5) modal5.style.display = "none";
+  else if(event.target==modal6) modal6.style.display = "none";
 }
